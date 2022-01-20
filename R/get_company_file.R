9a24b134d809d507be699b25c3c6d638816ef0e7
Harry Alexander <harry.alexander@mango-solutions.com>
2022-01-20 12:17:56 +0000

    Arguments now have default values matching the build_url function, new roxygen headers for each parameter, removed blank line at bottom of file, general tidying

--- R/get_company_file.R
+++ R/get_company_file.R
@@ -5,7 +5,9 @@
 #' and returns data about the corresponding file from the BambooHR API.
 #'
 #' @param file_id The ID of the file to get from BambooHR.
-#' @param ... The company domain, API version, and base url.
+#' @param company_domain The company's domain name
+#' @param api_version version of the BambooHR API
+#' @param base_url base url to the API gateway
 #'
 
 #' @return returns a response object.
@@ -21,18 +23,21 @@
 #'
 #'@author Mark Druffel, \email{mdruffel@propellerpdx.com}
 
+get_company_file <- function(file_id,
+                             company_domain = .get_company_name(),
+                             api_version = "v1",
+                             base_url = "https://api.bamboohr.com/api/gateway.php"){
 
-get_company_file <- function(file_id, company_domain, api_version, base_url){
-  #Default to directory if an individual employee is not specified
-  file_id <- rlang::maybe_missing(file_id, default = "view")
-  #Call the `build_url` function parsing in each element of the dots list
-  url <- build_url(company_domain = company_domain,
-                   api_version = api_version,
-                   base_url = base_url)
-  #Glues "/files/file_id" to url returned from build_url call
-  url <- glue::glue("{url}/files/{file_id}")
-  response <- get_request(url)
-  return(response)
+    # Default to directory if an individual employee is not specified
+    file_id <- rlang::maybe_missing(file_id, default = "view")
+    # Call the `build_url` function parsing in each element of the dots list
+    url <- build_url(
+      company_domain = company_domain,
+      api_version = api_version,
+      base_url = base_url
+    )
+    # Glues "/files/file_id" to url returned from build_url call
+    url <- glue::glue("{url}/files/{file_id}")
+    response <- get_request(url)
+    return(response)
 }
-
-
